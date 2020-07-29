2228b3e143b2f67924390d5b86a928b58137d3f9
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-29 17:41:32 +0300

    refactor(azure): refactor azure containerGroup rest service

--- app/azure/rest/container_group.js
+++ app/azure/rest/container_group.js
@@ -1,48 +1,39 @@
-angular.module('portainer.azure').factory('ContainerGroup', [
-  '$resource',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  function ContainerGroupFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
-    'use strict';
+import angular from 'angular';
 
-    var resource = {};
+angular.module('portainer.azure').factory('ContainerGroup', ContainerGroupFactory);
 
-    var base = $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/azure/subscriptions/:subscriptionId/providers/Microsoft.ContainerInstance/containerGroups',
-      {
-        endpointId: EndpointProvider.endpointID,
-        'api-version': '2018-04-01',
-      },
-      {
-        query: { method: 'GET', params: { subscriptionId: '@subscriptionId' } },
-      }
-    );
+function ContainerGroupFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
+  const base = $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/azure/subscriptions/:subscriptionId/providers/Microsoft.ContainerInstance/containerGroups`,
+    {
+      endpointId: EndpointProvider.endpointID,
+      'api-version': '2018-04-01',
+    },
+    {
+      query: { method: 'GET', params: { subscriptionId: '@subscriptionId' } },
+    }
+  );
 
-    var withResourceGroup = $resource(
-      API_ENDPOINT_ENDPOINTS +
-        '/:endpointId/azure/subscriptions/:subscriptionId/resourceGroups/:resourceGroupName/providers/Microsoft.ContainerInstance/containerGroups/:containerGroupName',
-      {
-        endpointId: EndpointProvider.endpointID,
-        'api-version': '2018-04-01',
-      },
-      {
-        create: {
-          method: 'PUT',
-          params: {
-            subscriptionId: '@subscriptionId',
-            resourceGroupName: '@resourceGroupName',
-            containerGroupName: '@containerGroupName',
-          },
+  const withResourceGroup = $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/azure/subscriptions/:subscriptionId/resourceGroups/:resourceGroupName/providers/Microsoft.ContainerInstance/containerGroups/:containerGroupName`,
+    {
+      endpointId: EndpointProvider.endpointID,
+      'api-version': '2018-04-01',
+    },
+    {
+      create: {
+        method: 'PUT',
+        params: {
+          subscriptionId: '@subscriptionId',
+          resourceGroupName: '@resourceGroupName',
+          containerGroupName: '@containerGroupName',
         },
-        get: {
-          method: 'GET',
-        },
-      }
-    );
+      },
+      get: {
+        method: 'GET',
+      },
+    }
+  );
 
-    resource.query = base.query;
-    resource.create = withResourceGroup.create;
-    resource.get = withResourceGroup.get;
-    return resource;
-  },
-]);
+  return { query: base.query, create: withResourceGroup.create, get: withResourceGroup.get };
+}
