799cd374bde861000e1692d9fa7d7be7732190b8
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-29 19:02:39 +0300

    refactor(azure): resourceGroup service

--- app/azure/services/resourceGroupService.js
+++ app/azure/services/resourceGroupService.js
@@ -1,35 +1,23 @@
+import angular from 'angular';
+
 import { ResourceGroupViewModel } from '../models/resource_group';
 
-angular.module('portainer.azure').factory('ResourceGroupService', [
-  '$q',
-  'ResourceGroup',
-  function ResourceGroupServiceFactory($q, ResourceGroup) {
-    'use strict';
-    var service = {};
+angular.module('portainer.azure').factory('ResourceGroupService', ResourceGroupServiceFactory);
 
-    service.resourceGroups = function (subscriptionId) {
-      var deferred = $q.defer();
+function ResourceGroupServiceFactory(ResourceGroup) {
+  return { resourceGroups, resourceGroup };
 
-      ResourceGroup.query({ subscriptionId: subscriptionId })
-        .$promise.then(function success(data) {
-          var resourceGroups = data.value.map(function (item) {
-            return new ResourceGroupViewModel(item, subscriptionId);
-          });
-          deferred.resolve(resourceGroups);
-        })
-        .catch(function error(err) {
-          deferred.reject({ msg: 'Unable to retrieve resource groups', err: err });
-        });
-
-      return deferred.promise;
-    };
-
-    service.resourceGroup = resourceGroup;
-    async function resourceGroup(subscriptionId, resourceGroupName) {
-      const group = await ResourceGroup.get({ subscriptionId, resourceGroupName }).$promise;
-      return new ResourceGroupViewModel(group);
+  async function resourceGroups(subscriptionId) {
+    try {
+      const results = await ResourceGroup.query({ subscriptionId }).$promise;
+      return results.value.map((item) => new ResourceGroupViewModel(item, subscriptionId));
+    } catch (err) {
+      throw { msg: 'Unable to retrieve resource groups', err };
     }
+  }
 
-    return service;
-  },
-]);
+  async function resourceGroup(subscriptionId, resourceGroupName) {
+    const group = await ResourceGroup.get({ subscriptionId, resourceGroupName }).$promise;
+    return new ResourceGroupViewModel(group);
+  }
+}
