7d319746306b37c3fc278519be7f83e20134db7d
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-29 16:50:04 +0300

    refactor(app): remove angular deps injection

--- app/config.js
+++ app/config.js
@@ -1,71 +1,59 @@
+import angular from 'angular';
 import toastr from 'toastr';
 import { Terminal } from 'xterm';
 import * as fit from 'xterm/lib/addons/fit/fit';
 
-angular.module('portainer').config([
-  '$urlRouterProvider',
-  '$httpProvider',
-  'localStorageServiceProvider',
-  'jwtOptionsProvider',
-  '$uibTooltipProvider',
-  '$compileProvider',
-  'cfpLoadingBarProvider',
-  function ($urlRouterProvider, $httpProvider, localStorageServiceProvider, jwtOptionsProvider, $uibTooltipProvider, $compileProvider, cfpLoadingBarProvider) {
-    'use strict';
+angular.module('portainer').config(config);
 
-    var environment = '@@ENVIRONMENT';
-    if (environment === 'production') {
-      $compileProvider.debugInfoEnabled(false);
-    }
+function config($urlRouterProvider, $httpProvider, localStorageServiceProvider, jwtOptionsProvider, $uibTooltipProvider, $compileProvider, cfpLoadingBarProvider) {
+  var environment = '@@ENVIRONMENT';
+  if (environment === 'production') {
+    $compileProvider.debugInfoEnabled(false);
+  }
 
-    localStorageServiceProvider.setPrefix('portainer');
+  localStorageServiceProvider.setPrefix('portainer');
 
-    jwtOptionsProvider.config({
-      tokenGetter: [
-        'LocalStorage',
-        function (LocalStorage) {
-          return LocalStorage.getJWT();
-        },
-      ],
-    });
-    $httpProvider.interceptors.push('jwtInterceptor');
-    $httpProvider.interceptors.push('EndpointStatusInterceptor');
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
-    $httpProvider.defaults.headers.put['Content-Type'] = 'application/json';
-    $httpProvider.defaults.headers.patch['Content-Type'] = 'application/json';
+  jwtOptionsProvider.config({
+    tokenGetter: /* @ngInject */ function tokenGetter(LocalStorage) {
+      return LocalStorage.getJWT();
+    },
+  });
+  $httpProvider.interceptors.push('jwtInterceptor');
+  $httpProvider.interceptors.push('EndpointStatusInterceptor');
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+  $httpProvider.defaults.headers.put['Content-Type'] = 'application/json';
+  $httpProvider.defaults.headers.patch['Content-Type'] = 'application/json';
 
-    $httpProvider.interceptors.push([
-      'HttpRequestHelper',
-      function (HttpRequestHelper) {
-        return {
-          request: function (config) {
-            if (config.url.indexOf('/docker/') > -1) {
-              config.headers['X-PortainerAgent-Target'] = HttpRequestHelper.portainerAgentTargetHeader();
-              if (HttpRequestHelper.portainerAgentManagerOperation()) {
-                config.headers['X-PortainerAgent-ManagerOperation'] = '1';
-              }
+  $httpProvider.interceptors.push(
+    /* @ngInject */ function (HttpRequestHelper) {
+      return {
+        request: function (config) {
+          if (config.url.indexOf('/docker/') > -1) {
+            config.headers['X-PortainerAgent-Target'] = HttpRequestHelper.portainerAgentTargetHeader();
+            if (HttpRequestHelper.portainerAgentManagerOperation()) {
+              config.headers['X-PortainerAgent-ManagerOperation'] = '1';
             }
-            return config;
-          },
-        };
-      },
-    ]);
+          }
+          return config;
+        },
+      };
+    }
+  );
 
-    toastr.options.timeOut = 3000;
+  toastr.options.timeOut = 3000;
 
-    Terminal.applyAddon(fit);
+  Terminal.applyAddon(fit);
 
-    $uibTooltipProvider.setTriggers({
-      mouseenter: 'mouseleave',
-      click: 'click',
-      focus: 'blur',
-      outsideClick: 'outsideClick',
-    });
+  $uibTooltipProvider.setTriggers({
+    mouseenter: 'mouseleave',
+    click: 'click',
+    focus: 'blur',
+    outsideClick: 'outsideClick',
+  });
 
-    cfpLoadingBarProvider.includeSpinner = false;
-    cfpLoadingBarProvider.parentSelector = '#loadingbar-placeholder';
-    cfpLoadingBarProvider.latencyThreshold = 600;
+  cfpLoadingBarProvider.includeSpinner = false;
+  cfpLoadingBarProvider.parentSelector = '#loadingbar-placeholder';
+  cfpLoadingBarProvider.latencyThreshold = 600;
 
-    $urlRouterProvider.otherwise('/auth');
-  },
-]);
+  $urlRouterProvider.otherwise('/auth');
+}
