8ffe6db898c7dc5209dd8f3a65d7989b4d0060ed
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-29 17:57:27 +0300

    refactor(azure): refacotor resource group rest service

--- app/azure/rest/resource_group.js
+++ app/azure/rest/resource_group.js
@@ -1,19 +1,17 @@
-angular.module('portainer.azure').factory('ResourceGroup', [
-  '$resource',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  function ResourceGroupFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
-    'use strict';
-    return $resource(
-      API_ENDPOINT_ENDPOINTS + '/:endpointId/azure/subscriptions/:subscriptionId/resourcegroups/:resourceGroupName',
-      {
-        endpointId: EndpointProvider.endpointID,
-        'api-version': '2018-02-01',
-      },
-      {
-        query: { method: 'GET', params: { subscriptionId: '@subscriptionId' } },
-        get: { method: 'GET' },
-      }
-    );
-  },
-]);
+import angular from 'angular';
+
+angular.module('portainer.azure').factory('ResourceGroup', ResourceGroupFactory);
+
+function ResourceGroupFactory($resource, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
+  return $resource(
+    `${API_ENDPOINT_ENDPOINTS}/:endpointId/azure/subscriptions/:subscriptionId/resourcegroups/:resourceGroupName`,
+    {
+      endpointId: EndpointProvider.endpointID,
+      'api-version': '2018-02-01',
+    },
+    {
+      query: { method: 'GET', params: { subscriptionId: '@subscriptionId' } },
+      get: { method: 'GET' },
+    }
+  );
+}
