058e90e5c37a59fe6d49e77d24edd5312e3db0fe
Chaim Lev-Ari <chiptus@gmail.com>
2020-07-29 17:36:50 +0300

    refactor(azure): refactor Azure rest

--- app/azure/rest/azure.js
+++ app/azure/rest/azure.js
@@ -1,20 +1,16 @@
-angular.module('portainer.azure').factory('Azure', [
-  '$http',
-  'API_ENDPOINT_ENDPOINTS',
-  'EndpointProvider',
-  function AzureFactory($http, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
-    'use strict';
+import angular from 'angular';
 
-    var service = {};
+angular.module('portainer.azure').factory('Azure', AzureFactory);
 
-    service.delete = function (id, apiVersion) {
-      var url = API_ENDPOINT_ENDPOINTS + '/' + EndpointProvider.endpointID() + '/azure' + id + '?api-version=' + apiVersion;
-      return $http({
-        method: 'DELETE',
-        url: url,
-      });
-    };
+function AzureFactory($http, API_ENDPOINT_ENDPOINTS, EndpointProvider) {
+  return { delete: deleteItem };
 
-    return service;
-  },
-]);
+  function deleteItem(id, apiVersion) {
+    const endpointId = EndpointProvider.endpointID();
+    const url = `${API_ENDPOINT_ENDPOINTS}/${endpointId}/azure${id}?api-version=${apiVersion}`;
+    return $http({
+      method: 'DELETE',
+      url: url,
+    });
+  }
+}
