629c518501ea577880e86a87eb755ce2874ec9be
Chaim Lev-Ari <chiptus@gmail.com>
2021-01-25 11:49:29 +0200

    refactor(env-vars): rename files

--- app/portainer/components/environment-variables-panel/index.js
+++ app/portainer/components/environment-variables-panel/index.js
@@ -2,10 +2,10 @@ import angular from 'angular';
 
 import './environment-variables-panel.css';
 
-import controller from './environmentVariablesPanel.controller.js';
+import controller from './environment-variables-panel.controller.js';
 
 angular.module('portainer.app').component('environmentVariablesPanel', {
-  templateUrl: './environmentVariablesPanel.html',
+  templateUrl: './environment-variables-panel.html',
   controller,
   bindings: {
     envVars: '=',
